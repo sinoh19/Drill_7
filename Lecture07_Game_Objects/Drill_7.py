62bfbe55d87f1873bb0e6860209dfe986358dbe5
sinoh19 <60386845+sinoh19@users.noreply.github.com>
2025-09-30 19:21:37 +0900

    ball 클래스 구현

--- Lecture07_Game_Objects/Drill_7.py
+++ Lecture07_Game_Objects/Drill_7.py
@@ -2,3 +2,14 @@ from pico2d import *
 import random
 
 
+class Ball:
+    def __init__(self):
+        if random.randint(0,1) == 0:
+            self.image = load_image('ball21x21.png')
+            self.size = 21
+        else:
+            self.image = load_image('ball41x41.png')
+            self.size = 41
+
+        self.x = random.randint(0, 800)
+        self.y = random.randint(0, 600)
