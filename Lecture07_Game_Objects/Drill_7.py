4555679839e61ec31bf82cf71a28407399092d0f
sinoh19 <60386845+sinoh19@users.noreply.github.com>
2025-09-30 19:29:03 +0900

    기본적인 기능 구현 공이 랜덤한 크기로 떨어지고 바닥에 붙음

--- Lecture07_Game_Objects/Drill_7.py
+++ Lecture07_Game_Objects/Drill_7.py
@@ -56,4 +56,23 @@ def render_world():
         ball.draw()
     update_canvas()
 
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
 open_canvas()
+rest_world()
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.05)
+
+
+close_canvas()
